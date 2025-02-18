087b6616856870eec75ef440cf3620fd5731fe7a
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-02-17 21:27:55 -0300

    add colours and gamePatern array

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
+const buttonColours = ["red", "blue", "green", "yellow"];
+const gamePattern = [];
+
 function nextSequence() {
   let randomNumber = Math.floor(4 * Math.random());
   return randomNumber;
 }
+
+function addNewColour() {
+  const randomChosenColour = buttonColours[nextSequence()];
+  gamePattern.push(randomChosenColour);
+  console.log(randomChosenColour);
+  console.log(gamePattern);
+}
+
+function countColours(pattern) {
+  const colourCount = { red: 0, blue: 0, green: 0, yellow: 0 };
+
+  pattern.forEach((colour) => {
+    if (colourCount.hasOwnProperty(colour)) {
+      colourCount[colour]++;
+    }
+  });
+
+  return colourCount;
+}
+
+//console.log(randomChosenColour);
+
+for (let index = 0; index < 3; index++) {
+  addNewColour();
+}
+
+//console.log(gamePattern);
+//console.log(countColours(gamePattern));
